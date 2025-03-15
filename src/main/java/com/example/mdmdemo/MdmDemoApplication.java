2c53ea9621c47543ace6d26d44623b8f66574db8
autowired-bean <shiryaevam00@mail.ru>
2025-03-15 11:08:59 +0300

    init commit

---
+++ src/main/java/com/example/mdmdemo/MdmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mdmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdmDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mdmdemo/MdmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mdmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdmDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
